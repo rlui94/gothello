7c658a500ee45b0ed63450d021304b1f61e31e22
ryant <ryanlui.college@gmail.com>
2019-12-07 18:45:50 -0800

    Redid tests for digits/letters and constructor

--- test_move.py
+++ test_move.py
@@ -3,12 +3,16 @@ from move import Move
 
 
 class MyTestCase(unittest.TestCase):
-    def test_digit(self):
-        # mv = Move.from_coords(0, 2)
-        mv = Move("a3")
+    def test_constructor(self):
+        mv = Move.from_desc("a3")
+        self.assertEqual(mv.name(), "a3")
+        mv = Move(1, 1)
+        self.assertEqual(mv.name(), "b2")
+
+    def test_digits_letters(self):
+        mv = Move.from_desc("a3")
         self.assertEqual(mv.move_digit('1'), 0)
         self.assertEqual(mv.move_letter('c'), 2)
-        self.assertEqual(mv.name(), "a3")
 
 
 if __name__ == '__main__':
