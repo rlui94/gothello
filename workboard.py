0e2271ed1e9e861372a005e0a2347fbdea849267
ryant <ryanlui.college@gmail.com>
2019-12-08 14:13:59 -0800

    Initial minimax implementation

--- workboard.py
+++ workboard.py
@@ -1,6 +1,9 @@
 import board
 
 
+INF = pow(board.BOARD_SIZE, 2)
+
+
 class Workboard(board.Board):
     def __init__(self):
         self.best_move = None
@@ -25,3 +28,33 @@ class Workboard(board.Board):
                     ostones += 1
         return nstones - ostones
 
+    def minimax(self, depth, max_player):
+        if depth <= 0 or self.game_state == board.GAME_OVER:
+            return self.heval()
+        mvs = self.gen_moves()
+        if max_player:
+            max_eval = -INF
+            for mv in mvs:
+                scratch = Workboard()
+                scratch.copy(self)
+                status = scratch.try_move(mv)
+                if status == board.ILLEGAL_MOVE:
+                    raise Exception("unexpectedly illegal move")
+                if status == board.GAME_OVER:
+                    raise Exception("unexpectedly game over")
+                child_eval = scratch.minimax(depth-1, False)
+                max_eval = max(max_eval, child_eval)
+            return max_eval
+        else:
+            min_eval = INF
+            for mv in mvs:
+                scratch = Workboard()
+                scratch.copy(self)
+                status = scratch.try_move(mv)
+                if status == board.ILLEGAL_MOVE:
+                    raise Exception("unexpectedly illegal move")
+                if status == board.GAME_OVER:
+                    raise Exception("unexpectedly game over")
+                child_eval = scratch.minimax(depth - 1, True)
+                min_eval = min(min_eval, child_eval)
+            return min_eval
