d3944d07d669709e011caa5d9f12e5b406ea86af
ryant <ryanlui.college@gmail.com>
2019-12-08 17:23:55 -0800

    test for minimax and find_best_move

--- test_workboard.py
+++ test_workboard.py
@@ -9,6 +9,13 @@ number_grid = [[0, 1, 0, 0, 0],
                [2, 0, 2, 1, 2],
                ]
 
+cap_grid = [[0, 1, 0, 1, 1],
+            [1, 1, 2, 1, 1],
+            [0, 0, 2, 2, 2],
+            [0, 2, 0, 2, 0],
+            [2, 0, 2, 1, 2],
+            ]
+
 
 class WorkboardTests(unittest.TestCase):
     def test_constructor(self):
@@ -20,6 +27,16 @@ class WorkboardTests(unittest.TestCase):
         wb.grid = number_grid
         # blk first so 6-7=-1
         self.assertEqual(wb.heval(), -1)
+
+    def test_minimax(self):
+        wb = Workboard()
+        wb.grid = cap_grid
+        print(wb.minimax(3, True))
+
+    def test_find_best(self):
+        wb = Workboard()
+        wb.grid = cap_grid
+        print(wb.find_best_move(3).name())
 
 
 if __name__ == '__main__':
